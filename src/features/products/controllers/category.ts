23a70d11c11815be00ede72c9d2400b67486c3fe
Osaretin Frank <osaretin.frank10@gmail.com>
2025-03-14 02:49:45 +0100

    add category controllers

--- src/features/products/controllers/category.ts
+++ src/features/products/controllers/category.ts
@@ -24,3 +24,97 @@ export const createCategory = async (req: Request, res: Response) => {
     return
   }
 }
+
+export const getCategory = async (req: Request, res: Response) => {
+  try {
+    const { title } = req.params
+    const category = await categoryModel.findOne({ title }).populate("products")
+    if (!category) {
+      res.status(404).json({ success: false, message: "Category not found" })
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Category fetched successfully",
+        category,
+      })
+    }
+    return
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error?.message })
+    return
+  }
+}
+
+export const getCategories = async (req: Request, res: Response) => {
+  try {
+    const categories = await categoryModel.find()
+    if (!categories) {
+      res.status(404).json({ success: false, message: "Categories not found" })
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Categories fetched successfully",
+        categories,
+      })
+    }
+    return
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error?.message })
+    return
+  }
+}
+
+export const updateCategory = async (req: Request, res: Response) => {
+  try {
+    const { title, preview_image } = req.body as {
+      title: string
+      preview_image?: File
+    }
+    const { id } = req.params
+    const category = await categoryModel.findById(id)
+    if (!category) {
+      res.status(404).json({ success: false, message: "Category not found" })
+    } else {
+      if (!preview_image) {
+        await category.updateOne({ title })
+        const newCategory = await category.save()
+        res.status(201).json({
+          success: true,
+          message: "Category updated",
+          category: newCategory,
+        })
+      } else {
+        await category.updateOne({ title })
+        await category.save()
+        res
+          .status(201)
+          .json({ success: true, message: "Category updated", category })
+      }
+    }
+
+    return
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error?.message })
+    return
+  }
+}
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const category = await categoryModel.findById(id)
+    if (!category) {
+      res.status(404).json({ success: false, message: "Category not found" })
+    } else {
+      await category.deleteOne()
+      res.status(200).json({
+        success: true,
+        message: "Category deleted successfully",
+      })
+    }
+    return
+  } catch (error: any) {
+    res.status(400).json({ success: false, message: error?.message })
+    return
+  }
+}
--- src/features/products/router/index.ts
+++ src/features/products/router/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express"
-import { createCategory } from "../controllers/category"
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+} from "../controllers/category"
 import { auth } from "../../../middleware/auth"
 import { forAdmin } from "../../../middleware/forAdmin"
 export const productRouter = Router()
@@ -7,3 +13,7 @@ export const categoryRouter = Router()
 
 // CATEGORY ROUTES
 categoryRouter.post("/", auth, forAdmin, createCategory)
+categoryRouter.put("/:id", auth, forAdmin, updateCategory)
+categoryRouter.delete("/:id", auth, forAdmin, deleteCategory)
+categoryRouter.get("/:title", getCategory)
+categoryRouter.get("/", getCategories)
