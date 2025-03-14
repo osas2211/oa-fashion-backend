3b59154d0a1d33f850600c6b9bac24e496c52839
Osaretin Frank <osaretin.frank10@gmail.com>
2025-03-14 23:47:55 +0100

    add updates

--- src/features/products/controllers/product.ts
+++ src/features/products/controllers/product.ts
@@ -152,6 +152,7 @@ export const updateProduct = async (req: Request, res: Response) => {
         ...textPayload,
         image: preview_image_url || product.image,
       })
+      await updatedProduct.save()
 
       res.status(201).json({
         success: true,
@@ -173,6 +174,7 @@ export const updateProduct = async (req: Request, res: Response) => {
         image: preview_image_url || product.image,
         subImages: uploadResults || product.subImages,
       })
+      await updatedProduct.save()
 
       res.status(201).json({
         success: true,
